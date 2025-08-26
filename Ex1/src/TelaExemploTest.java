75be2650bda74b51a139b7fecdad82032317594a
Pedro Wilian <pedrowbevilacqua@gmail.com>
2025-08-26 13:42:42 -0300

    Adcionando exercicios de LP1 - Aula 15

--- Ex1/src/TelaExemploTest.java
+++
@@ -1,9 +0,0 @@
-
-import javax.swing.JFrame;
-
-public class TelaExemploTest {
-    public static void main(String[] args) {
-        TelaExemplo exemplo = new TelaExemplo();
-        exemplo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
--- Ex2/src/Main.java
+++
@@ -1,9 +0,0 @@
-import gui.TelaIdioma;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new TelaIdioma().setVisible(true);
-        });
-    }
-}
